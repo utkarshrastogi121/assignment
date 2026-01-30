84d1fbeee256c0407651c87460bd0a5c4d30743a
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-01-30 20:51:52 +0530

    role middleware added

---
+++ backend/src/middleware/role.middleware.ts
@@ -0,0 +1,10 @@
+import type { Response, NextFunction } from "express";
+import type { AuthRequest } from "./auth.middleware.js";
+
+export const roleMiddleware = (role: "ADMIN" | "STUDENT") => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    if (req.user?.role !== role)
+      return res.status(403).json({ message: "Forbidden" });
+    next();
+  };
+};
