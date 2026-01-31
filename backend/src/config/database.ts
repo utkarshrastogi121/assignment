f54499e16fa1e9f0fcb108cabd8adfb570ea5a00
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-01-31 11:01:51 +0530

    complete backend added

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log("DB URL:", process.env.DATABASE_URL);
+
 const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: "postgres",
   dialectOptions: {
