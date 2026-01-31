9d764afa93af2ac02bc4e16b9f4441a8f9e6f1c6
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-01-31 11:15:27 +0530

    frontend added

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { InternalAxiosRequestConfig } from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
